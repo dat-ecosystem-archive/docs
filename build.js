0a2b173001e9a0e437dcee9d79c9a077d1efd7ad
Joe Hand <joe@joeahand.com>
2016-08-29 12:50:09 -0700

    move ecosystem-docs files to modules folder

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var ndjson = require('ndjson')
 
+fs.mkdirSync('docs/modules')
 process.stdin.pipe(ndjson.parse()).on('data', function (obj) {
-  fs.writeFileSync('docs/' + obj.name + '.md', obj.readme)
+  fs.writeFileSync('docs/modules/' + obj.name + '.md', obj.readme)
 })

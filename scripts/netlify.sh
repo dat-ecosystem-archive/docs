1ec3a87db2db310ae3301ce6ba9525603f51107c
Joe Hand <joe@joeahand.com>
2017-02-03 13:31:43 -0800

    remove travis stuff

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 echo "Setting Git Stuff"
+# Need this for ecosystem-docs
 git config --global user.email "robot@overlord.industries"
 git config --global user.name "Netlify"
-git remote rm origin
-git remote add origin https://joehand:${GH_TOKEN}@github.com/datproject/docs.git
 
 echo "Running npm netlify"
 npm run netlify

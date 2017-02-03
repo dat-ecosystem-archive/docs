952b3ef5c93c9c5e7d582645fc5e3512e21318de
Joe Hand <joe@joeahand.com>
2017-02-03 13:18:53 -0800

    update netlify script

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Setting Git Stuff"
 git config --global user.email "robot@overlord.industries"
 git config --global user.name "Netlify"
 git remote rm origin
 git remote add origin https://joehand:${GH_TOKEN}@github.com/datproject/docs.git
-npm install
+
+echo "Running npm netlify"
 npm run netlify
